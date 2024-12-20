96362afcf206823e96319d4187b00036395341df
RaymondCostales <raymond.w.costales@gmail.com>
2024-12-20 15:37:59 -0700

    Stub and test item entity

---
+++ JPAdayz/src/test/java/com/skilldistillery/dayz/entities/ItemTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.dayz.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class ItemTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Item item;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAdayz");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		item = em.find(Item.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	void test_Item_entity_mapping() {
+//		assertNull(item);
+		assertNotNull(item);
+		assertEquals("Bandage", item.getName());
+	}
+
+}
