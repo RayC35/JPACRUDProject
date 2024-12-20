f43f013c6a056e50010cb330e42aace45ad36bb4
RaymondCostales <raymond.w.costales@gmail.com>
2024-12-20 16:06:30 -0700

    Create Boot MVC projcet, link JPA project dependency

---
+++ DayZGear/src/main/java/com/skilldistillery/dayz/DayZGearApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayZGearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayZGearApplication.class, args);
+	}
+
+}
---
+++ DayZGear/src/test/java/com/skilldistillery/dayz/DayZGearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayZGearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
